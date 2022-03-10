4ced58b0a8536e5dfa78d16e3157780043e09b2d
edrickl25 <edrickl25@gmail.com>
2022-03-09 20:40:53 -0500

    creating state map w capitals

--- 2_4_geographic/main.js
+++ 2_4_geographic/main.js
@@ -1,7 +1,7 @@
 /* CONSTANTS AND GLOBALS */
-// const width = window.innerWidth * 0.9,
-//   height = window.innerHeight * 0.7,
-//   margin = { top: 20, bottom: 50, left: 60, right: 40 };
+   const width = window.innerWidth * 0.9,
+     height = window.innerHeight * 0.7,
+     margin = { top: 20, bottom: 50, left: 60, right: 40 };
 
 /**
  * LOAD DATA
@@ -12,15 +12,51 @@ Promise.all([
   d3.csv("../data/stateCapitals.csv", d3.autoType),
 ]).then(([geojson, capitals]) => {
   
+
   // SPECIFY PROJECTION
- 
+  const projection = d3.geoAlbersUsa().fitSize([
+    width - margin.left - margin.right, height - margin.top - margin.bottom
+  ], geojson)
+
+  // CREATE SVG ELEMENT
+  const svg = d3.select("#container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+
+
+
 
   // DEFINE PATH FUNCTION
+  const pathGen = d3.geoPath(projection)
+   
+  //SELECTALL-DATA-JOIN
+  // for TWO data sets this time, so 2 JOINS
 
+  const states = 
+  svg.selectAll("path")
+    .data(geojson.features)
+    .join("path")
+    .attr("d", d => pathGen(d))
+    .attr("fill","magenta")
+    .attr("stroke","black")
 
+svg.selectAll("circle")
+  .data(capitals)
+  .join("circle")
+  .attr("r",2)
+  .attr("fill","gold")
+  .attr("transform", d => {
+    const [x, y] = projection([d.longitude, d.latitude])
+    return `translate(${x}, ${y})`
+  })
+  // First DRAW GEOJSON PATH via SELECT-DATA-JOIN for US States
+  
   // APPEND GEOJSON PATH  
   
   
-  // APPEND DATA AS SHAPE
+  // SELECT-DATA-JOIN-DRAW-ATTR to APPEND DATA AS SHAPE
 
+  // OPTION
+  // draw a point directly thorugh your code
 });
