f1575fcc3ceaeed1b66942a2510630d01775e8e9
edrickl25 <edrickl25@gmail.com>
2022-03-15 13:27:41 -0400

    fixed axis, good to go

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -1,16 +1,53 @@
 /* CONSTANTS AND GLOBALS */
-const width = window.innerWidth * .08;
-const height = window.innerHeight / 3;
+const width = window.innerWidth * .5;
+const height = window.innerHeight * .75;
+margin = 50;
 
 /* LOAD DATA */
 d3.csv('../data/squirrelActivities.csv',d3.autoType).then
 (data => {
   console.log("data", data)
 
-    /* SCALES */
-    /** This is where you should define your scales from data to pixel space */
-    
+  /* SCALES */
+  // xscale - categorical, activity
+  const xScale = d3.scaleBand()
+    .domain(data.map(d=> d.activity))
+    .range([margin,width]) // visual variable
+    .paddingInner(.2)
 
-    /* HTML ELEMENTS */
-    /** Select your container and append the visual elements to it */
-  })
+    // yscale - linear,count
+  const yScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d=> d.count)])
+    .range([height-margin, margin])
+
+  /* HTML ELEMENTS */
+  
+  // svg
+  const svg = d3.select("#container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+
+  const xAxis = d3.axisBottom(xScale)
+  const yAxis = d3.axisLeft(yScale)
+
+  svg.append("g")
+    .attr("transform", `translate(0,${height-margin})` )
+    .call(xAxis)
+
+  svg.append("g")
+    .attr("transform",`translate(${margin},0)`)
+    .call(yAxis)  
+
+  // bars
+  svg.selectAll("rect")
+    .data(data)
+    .join("rect")
+    .attr("width", xScale.bandwidth())
+//  .attr("width", d => width - margin - xScale(d.activity))
+    .attr("height", d=> height-margin - yScale(d.count))
+    .attr("x", d=> xScale(d.activity))
+    .attr("y", d=> yScale(d.count))
+
+})
+
